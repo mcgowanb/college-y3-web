197719efc19d5485c740d7f0fd7aeedaba5726b0
Brian McGowan <mcgowan.b@gmail.com>
2017-09-25 17:32:38 +0100

    classwork

--- sample/src/app/app.module.ts
+++ sample/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -16,7 +17,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
